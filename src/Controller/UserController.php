b5f893a4313384b5e19ba9235c5c219c02bdbeeb
GongoraLeo <gongora.leo@gmail.com>
2025-01-16 13:06:25 +0100

    bbdd doctrine

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,27 +2,38 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class UserController extends AbstractController{
-    // #[Route('/user', name: 'app_user')]
-    // public function index(): Response
-    // {
-    //     return $this->render('user/index.html.twig', [
-    //         'controller_name' => 'UserController',
-    //     ]);
-    // }
-
-    public function notifications(): Response
+    #[Route('/user', name: 'create_user')]
+    public function createUser(EntityManagerInterface $entityManager): Response
     {
-        $userFirstName = '...';
-        $userNotifications = ['...', '...'];
+        $user = new User();
+        $user->setName('Leonardo');
+        $user->setLastName('Gongora');
+        $user->setNickName('leon_noel');
 
-        return $this->render('user/notifications.html.twig', [
-            'user_first_name' => $userFirstName,
-            'notifications' => $userNotifications,
-        ]);
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new Response('Se ha creado un nuevo usuario '.$user->getName(). ', con la id '.$user->getId());
+    }
+
+    #[Route('/user/{id}', name: 'user_show')]
+    public function show(EntityManagerInterface $entityManager, int $id): Response
+    {
+        $user = $entityManager->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException(
+                'No se encuentra el usuario con el id: '.$id
+            );
+        }
+
+        return new Response('Hemos encontrado al usuario: '.$user->getName());
     }
 }
