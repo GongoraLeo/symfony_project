5e8c08a6a9f6d694b77a296254891143ea546c66
GongoraLeo <gongora.leo@gmail.com>
2025-01-24 12:23:18 +0100

    añadido registro y login de usuarios

--- src/Controller/InicioController.php
+++ src/Controller/InicioController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,6 +12,16 @@ final class InicioController extends AbstractController
     #[Route('/', name: 'homepage')]
     public function index(): Response
     {
-        return $this->render('inicio/index.html.twig');
+        $user = $this->getUser();
+
+        //hay que redireccionar a la pagina de login de usuario app_login
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }else{
+            return $this->render('inicio/index.html.twig');
+        }
+
+        
+        // return $this->render('inicio/index.html.twig');
     }
 }
